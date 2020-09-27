05fdd05446c708a59fa66dbf8f31d221fc3e6332
PARET Paul <p.paret@rubika-edu.com>
2020-09-27 17:39:37 +0200

    Hitstun effect when cars collides

--- FLIPPER Project/Assets/Scripts/Player/AlternativePlayerBounce.cs
+++ FLIPPER Project/Assets/Scripts/Player/AlternativePlayerBounce.cs
@@ -8,9 +8,14 @@ public class AlternativePlayerBounce : MonoBehaviour
 
     [SerializeField] float bounceStrength = 1;
 
+    [Header("Stun Effect")]
+
+    [SerializeField] private float stunDuration = 0.2f;
+
     [Header("Dev Requierement")]
 
     [SerializeField] Rigidbody2D carRgb = null;
+    private PlayerMovementScript playerMovementScript;
 
     //Layer infos:
     LayerMask obstaclesMask;
@@ -19,6 +24,7 @@ public class AlternativePlayerBounce : MonoBehaviour
     private void Start()
     {
         playerMask = LayerMask.NameToLayer("Player");
+        playerMovementScript = GetComponent<PlayerMovementScript>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -27,8 +33,28 @@ public class AlternativePlayerBounce : MonoBehaviour
         {
             Debug.Log("player collision");
             carRgb.AddForce((gameObject.transform.position - collision.gameObject.transform.position).normalized * bounceStrength);
+
+            StartCoroutine(hitStun());
         }
     }
 
+    /// <summary>
+    /// Effet appliquer quand les joueurs entre en collision
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator hitStun()
+    {
+        playerMovementScript.canMoveHorizontal = false;
+        playerMovementScript.canTurbo = false;
+        playerMovementScript.canBreak = false;
+        playerMovementScript.canStraff = false;
+
+        yield return new WaitForSecondsRealtime(stunDuration);
+
+        playerMovementScript.canMoveHorizontal = true;
+        playerMovementScript.canTurbo = true;
+        playerMovementScript.canBreak = true;
+        playerMovementScript.canStraff = true;
+    }
 
 }
