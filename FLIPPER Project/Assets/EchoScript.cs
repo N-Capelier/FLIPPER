7ca40d1b0a644a7de548149a2f9a173f79a6056b
PARET Paul <p.paret@rubika-edu.com>
2020-09-27 17:24:21 +0200

    Car Tilt + Straff Meca + TrailEffect + Echo Effect

---
+++ FLIPPER Project/Assets/EchoScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EchoScript : MonoBehaviour
+{
+    public bool canEcho;
+
+    public GameObject echoGO;
+
+    private float timeBtwSpawn = 0f;
+    [SerializeField] private float startTimeBtwSpawn = 0.1f;
+
+    private void Update()
+    {
+        if (canEcho)
+        {
+            if (timeBtwSpawn <= 0)
+            {
+                //spawn echo game object
+                GameObject echoInstance = Instantiate(echoGO, transform.position, Quaternion.identity);
+                Destroy(echoInstance, 3f);
+                timeBtwSpawn = startTimeBtwSpawn;
+            }
+            else
+            {
+                timeBtwSpawn -= Time.deltaTime;
+            }
+        }
+        
+    }
+}
