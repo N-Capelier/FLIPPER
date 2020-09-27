740dbab73a1f3cc37c627bf94d7cd4249ecc6f93
Bastien <B.prigent@rubika-edu.com>
2020-09-27 12:42:14 +0200

    PRBA_UI
    Fuel UI Works

---
+++ FLIPPER Project/Assets/Scripts/UI/UIScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour
+{
+    [SerializeField] private Image fuel;
+    [SerializeField] private PlayerMovementScript plMoveScript;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        plMoveScript = GetComponent<PlayerMovementScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fuel.fillAmount = plMoveScript.turboFuel/100f;
+    }
+}
