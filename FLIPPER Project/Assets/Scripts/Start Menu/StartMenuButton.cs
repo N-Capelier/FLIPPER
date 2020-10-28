e9e3b452b2d1bb0cc6071eb1d9f97e9bb894ceb2
Rémi Sécher <r.secher@rubika-edu.com>
2020-10-28 15:53:05 +0100

    End Feature

--- FLIPPER Project/Assets/Scripts/Start Menu/StartMenuButton.cs
+++ FLIPPER Project/Assets/Scripts/Start Menu/StartMenuButton.cs
@@ -2,36 +2,102 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class StartMenuButton : MonoBehaviour
 {
-    public GameObject buttonText = null;
-    public float buttonTextScaleValue = 1;
-    public Vector3 buttonTextScaleVector = new Vector3(1, 1, 1);
-    public bool animate = false;
-    public float animationStep = 0.5f;
-    public float animationLimMin = 1.5f;
-    public float animationLimMax = 0.5f;
-    RectTransform buttonTextRT = null;
+    [Header("Requiered Data")]
 
-    // Start is called before the first frame update
-    void Start()
+    public GameObject buttonText = null;
+    public EventSystem StartMenuEventSystem = null;
+    RectTransform buttonTextRT = null;
+    Button thisButtonComp = null;
+
+    [Header("Global Behaviour Management")]
+
+    public bool animate = true;
+
+    bool isReset = true;
+
+    [Header("Scale Animation Values")]
+     
+    public float animationScaleStep = 0.5f;
+    public float animationScaleLimMin = 0.5f;
+    public float animationScaleLimMax = 1.5f;
+
+    float buttonTextScaleValue = 1;
+    bool grow = true;
+
+    [Header("Rotation Animation Values")]
+
+    public float animationRotateStep = 0.1f;
+    public float animationRotateLimMin = -5f;
+    public float animationRotateLimMax = 5f;
+
+    float buttonTextRotateValue = 0;
+
+    void Awake()
     {
         buttonTextRT = buttonText.GetComponent<RectTransform>();
+        thisButtonComp = GetComponent<Button>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (animate)
+        if (StartMenuEventSystem.currentSelectedGameObject == gameObject && animate == true)
         {
-            if()
+            if (isReset) isReset = false;           
             SelectedAnimation();
+        }
+        else if (!isReset)      //Reset Button Text at default value on unSelected
+        {
+            buttonTextScaleValue = 1f;
+            buttonTextRT.localScale = new Vector3(buttonTextScaleValue, buttonTextScaleValue, buttonTextScaleValue);
+
+            buttonTextRotateValue = 0;
+            buttonTextRT.rotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+    void SelectedAnimation()
+    {
+        if (grow)
+        {
+            if ( buttonTextScaleValue < animationScaleLimMax - 0.1f * animationScaleStep)
+            {
+                buttonTextScaleValue = Mathf.Lerp(buttonTextScaleValue, animationScaleLimMax, animationScaleStep * Time.deltaTime);
+                buttonTextRT.localScale = new Vector3(buttonTextScaleValue, buttonTextScaleValue, buttonTextScaleValue);
+
+                buttonTextRotateValue = Mathf.Lerp(buttonTextRotateValue, animationRotateLimMax, animationRotateStep * Time.deltaTime);
+                buttonTextRT.rotation = Quaternion.Euler(0, 0, buttonTextRotateValue);
+            }
+            else
+            {
+                grow = false;
+            }
+        }
+        else
+        {
+            if (buttonTextScaleValue > animationScaleLimMin + 0.1f * animationScaleStep)
+            {
+                buttonTextScaleValue = Mathf.Lerp(buttonTextScaleValue, animationScaleLimMin, animationScaleStep * Time.deltaTime);
+                buttonTextRT.localScale = new Vector3(buttonTextScaleValue, buttonTextScaleValue, buttonTextScaleValue);
+
+                buttonTextRotateValue = Mathf.Lerp(buttonTextRotateValue, animationRotateLimMin, animationRotateStep * Time.deltaTime);
+                buttonTextRT.rotation = Quaternion.Euler(0, 0, buttonTextRotateValue);
+            }
+            else
+            {
+                grow = true;
+            }
         }
     }
 
-    void SelectedAnimation()
+    public void SwitchButton(bool activated)
     {
-         
+        if (gameObject.activeInHierarchy)
+        {
+            thisButtonComp.interactable = activated;
+            animate = activated;
+        }
     }
 }
---
+++ FLIPPER Project/Assets/Scripts/Start Menu/StartMenuManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartMenuManager : MonoBehaviour
+{
+    public EventSystem mainMenuEventSystem = null;
+
+    GameObject lastSelectedGameObject = null;
+
+    public StartMenuButton[] allMenuButton = null;
+
+    public float startAnimationTime = 1.5f;
+
+    private void Start()
+    {
+        if(mainMenuEventSystem != null)
+        {
+            lastSelectedGameObject = mainMenuEventSystem.firstSelectedGameObject;
+        }
+
+        mainMenuEventSystem.gameObject.SetActive(false);
+
+        foreach(StartMenuButton button in allMenuButton)
+        {
+            button.SwitchButton(false);
+        }
+
+        StartCoroutine(ActivateMenu(startAnimationTime));
+    }
+
+    private void Update()
+    {
+        if(mainMenuEventSystem != null)
+        {
+            if (mainMenuEventSystem.currentSelectedGameObject == null)
+            {
+                mainMenuEventSystem.SetSelectedGameObject(lastSelectedGameObject);
+            }
+            else
+            {
+                lastSelectedGameObject = mainMenuEventSystem.currentSelectedGameObject;
+            }
+        }      
+    }
+
+    IEnumerator ActivateMenu(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        foreach (StartMenuButton button in allMenuButton)
+        {
+            button.SwitchButton(true);
+        }
+
+        mainMenuEventSystem.gameObject.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Settings()
+    {
+
+    }
+
+    public void Credits()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
